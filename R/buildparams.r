ad6c97426e62ab0d3074561fa02a49b5cf2e9dce
janjansen86 <jansen.jan@web.de>
2016-06-21 12:18:16 +1000

    sinking speed bug in buildparams fixed

--- R/buildparams.r
+++ R/buildparams.r
@@ -18,8 +18,8 @@ buildparams <- function(speed,
   K =0.0025,            #drag coefficient
   E =1,                #aspect ration of settling flocks (spherical = 1 ??)
   r =0.00016,           #particle-radius
-  Wd =speed,#/24/3600,
   Ucsq =-(0.05*(p0-p1)*g*2*(1.5*E)^(1/3)*r)/(p0*K),
+  Wd =speed,#/24/3600,
   testFunct =function(U_div,dens) 1800*-(p1*(dens)*Wd*cos(90)*(U_div)*(U_div))/p0){
   list(p0 = p0, p1 = p1, cosO = cosO, g = g, K = K, E = E, r = r, Wd = Wd, Ucsq = Ucsq, testFunct = testFunct)
 }
