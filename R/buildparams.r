0a70e85d78500d3659d28dc8eed398879ca8961c
Jan Jansen <Jan.Jansen@utas.edu.au>
2017-11-10 11:38:00 +1100

    edit buildparams sedimentation  function

--- R/buildparams.r
+++ R/buildparams.r
@@ -13,14 +13,14 @@ buildparams <- function(speed,
   ## from Jenkins & Bombosch (1995)
   p0 =1030,             #kg/m^3 seawater density
   p1 =1100,             #kg/m^3 Diatom density (so far a quick-look-up-average density from Ierland & Peperzak (1984))
-  cosO =1,              #its 1 for 90degrees
+  cosO =1,              #
   g =9.81,              #accelaration due to gravity
   K =0.0025,            #drag coefficient
   E =1,                #aspect ration of settling flocks (spherical = 1 ??)
   r =0.00016,           #particle-radius
   Ucsq =-(0.05*(p0-p1)*g*2*(1.5*E)^(1/3)*r)/(p0*K),
   Wd =speed,#/24/3600,
-  SedFunct =function(U_div,dens) 1800*-(p1*(dens)*Wd*cos(90)*(U_div)*(U_div))/p0){
+  SedFunct =function(U_div,dens) 1800*(p1*(dens)*Wd*(U_div)*(U_div))/p0){
   list(p0 = p0, p1 = p1, cosO = cosO, g = g, K = K, E = E, r = r, Wd = Wd, Ucsq = Ucsq, SedFunct = SedFunct)
 }
 
